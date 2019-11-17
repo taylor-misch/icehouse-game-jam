10675edcda6eaa6c4c48a9c6655c39aa5861613e
TMisch <taylormisch@gmail.com>
2019-11-16 19:15:33 -0600

    Add player health and damage scripts/Add health scene

--- Assets/Scripts/HasHealth.cs
+++ Assets/Scripts/HasHealth.cs
@@ -2,34 +2,29 @@
 using System.Collections;
 
 public class HasHealth : MonoBehaviour {
-    [SerializeField] private float hitPoints = 60f;
+    [SerializeField] private float maxHealth = 60f;
+    private float currentHealth;
     [SerializeField] private bool isAlive;
-    [SerializeField] private float points = 100f;
-    [SerializeField] private AudioClip[] deathClip;		// The sound effect of the enemy dying.
+    [SerializeField] private AudioClip[] deathClip; // The sound effect of the enemy dying.
     [SerializeField] private AudioClip[] injuredClips;
     Animator _anim;
     private float timeToDie = 7f;
 
-    void Awake()    {
+    void Awake() {
         _anim = GetComponent<Animator>();
         isAlive = true;
+        currentHealth = maxHealth;
     }
 
-    void ApplyDamage(float damage, bool wasHeadShot)    {
-        hitPoints -= damage;
-        if (hitPoints <= 0 && isAlive) {
-        Die(wasHeadShot);
+    public void ChangeHealth(float amount) {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0f, maxHealth);
+        Debug.Log($"{currentHealth}/{maxHealth}");
+        if (currentHealth <= 0 && isAlive) {
+            Die();
         }
     }
 
-    public void RecieveDamage(float amt, bool wasHeadShot) {
-        hitPoints -= amt;
-        if (hitPoints <= 0 && isAlive) {
-            Die(wasHeadShot);
-        }
-    }
-
-    void Die(bool wasHeadShot) {
+    void Die() {
         _anim.SetTrigger("isDead");
         AudioSource.PlayClipAtPoint(deathClip[Random.Range(0, deathClip.Length)], gameObject.transform.position, .8f);
         Destroy(gameObject, timeToDie);
@@ -37,8 +32,6 @@ public class HasHealth : MonoBehaviour {
         //Blow up object 
         // set isAlive boolean to false
         isAlive = false;
-        float pointsToIncrease = wasHeadShot?points * 2: points;
-        GameManagement.manage.increaseScore(pointsToIncrease);
         GameManagement.manage.increaseKills();
     }
 }
